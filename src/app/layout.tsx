9dd8c3b34858480e94808a4e6ef83abeccdbebaf
Aleksander Chernov <aleksander@yallamart.ae>
2025-06-06 19:29:12 +0300

    feat: googleAnalytics chore: visual changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,6 +23,8 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const isProd = process.env.NODE_ENV === "production";
+  const gaId = process.env.GOOGLE_ANALYTICS ?? "";
   return (
     <html lang="en">
       <body
@@ -29,6 +32,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      {isProd && <GoogleAnalytics gaId={gaId} />}
     </html>
   );
 }
